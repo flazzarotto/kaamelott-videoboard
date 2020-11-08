1278b2446dd9e5bf18ab115f68f203778a8e9e6b
kebab-case <fabrice.lazzarotto@gmail.com>
2020-11-08 20:57:36 +0100

    fix episode parser

--- src/lib/functions/episodeParser.js
+++ src/lib/functions/episodeParser.js
@@ -4,9 +4,9 @@
  * @returns {string[]}
  */
 export const episodeParser = (episode) => {
-    episode = /^(L[0-9])(T[0-9])(E[0-9]+)$/g.exec(episode || '')
+    episode = /^(L[0-9])(T[0-9])?(E[0-9]+)?$/g.exec(episode || '')
     if (!episode) {
         return [null,null,null]
     }
-    return episode.slice(1)
+    return episode.slice(1).map(x => x ?? null)
 }
